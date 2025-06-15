99b2d39806196867e7fee4bb4241f495c9e030fb
omardtl24 <otoledo@unal.edu.co>
2025-06-15 17:07:13 -0500

    Practica 7 - Redes neuronales

--- AI_algorithms/utils/plot/__init__.py
+++ AI_algorithms/utils/plot/__init__.py
@@ -1,7 +1,8 @@
-from .visualize import plot_data, plot_decision_region, plot_losses
+from .visualize import plot_data, plot_decision_region, plot_losses, plot_decision_region_3d
 
 __all__ = [
     "plot_data",
     "plot_decision_region",
-    "plot_losses"
+    "plot_losses",
+    "plot_decision_region_3d"
     ]
